06017c4686fafa5008288fb423f50fb89a767372
Sathaphon_Wutthipanyasakul <sathaphon.wutthipanyasakul@gmail.com>
2023-12-19 12:15:25 +0700

    change exercise1

--- DOM Excercise/exercise1/script.js
+++ DOM Excercise/exercise1/script.js
@@ -1,6 +1,10 @@
 //Receive an input and print Hello the inputted name
-document.getElementById('button').addEventListener('click', function() {
+const buttonClick= document.getElementById('button');
+
+
+buttonClick.addEventListener('click', function() {
     const name = document.getElementById('input').value;
     document.querySelector('h1').innerHTML = `Hello, ${name}`;
 });
+
 
